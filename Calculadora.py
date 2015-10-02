5fcd7ac141b2c058cc17105a9f93347c6bcf1836
VaneAR <vdrar.06@gmail.com>
2015-10-02 10:51:16 -0500

    Archivos con pep8

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,7 @@
 class Calculadora():
-	def suma(self, num1, num2):
-		return num1 + num2
 
-	def resta(self, num1, num2):
-		return num1 - num2
+    def suma(self, num1, num2):
+        return num1 + num2
+
+    def resta(self, num1, num2):
+        return num1 - num2
--- TestCalculadora.py
+++ TestCalculadora.py
@@ -1,31 +1,32 @@
-import unittest;
+import unittest
 from Calculadora import Calculadora
 
+
 class TestCalculadora(unittest.TestCase):
-	def setUp(self):
-		self.calc = Calculadora()
 
-	def test_sumar_de_3_mas_2(self):
-		resultado = self.calc.suma(3,2)
-		self.assertEqual(5, resultado)
+    def setUp(self):
+        self.calc = Calculadora()
 
-	def test_sumar_de_3_mas_3(self):
-		resultado = self.calc.suma(3,3)
-		self.assertEqual(6, resultado)
+    def test_sumar_de_2_mas_2(self):
+        resultado = self.calc.suma(2, 2)
+        self.assertEqual(4, resultado)
 
-	def test_sumar_de_10_mas_10(self):
-		resultado = self.calc.suma(10,10)
-		self.assertEqual(20, resultado) 
+    def test_sumar_de_3_mas_3(self):
+        resultado = self.calc.suma(3, 3)
+        self.assertEqual(6, resultado)
 
-	def test_resta_de_5_menos_2(self):
-		resultado = self.calc.resta(5,2)
-		self.assertEqual(3,resultado)
+    def test_sumar_de_5_mas_5(self):
+        resultado = self.calc.suma(5, 5)
+        self.assertEqual(10, resultado)
 
-	def test_resta_de_2_menos_5(self):
-		resultado = self.calc.resta(2,5)
-		self.assertEqual(-3,resultado)
+    def test_resta_de_5_menos_2(self):
+        resultado = self.calc.resta(5, 2)
+        self.assertEqual(3, resultado)
+
+    def test_resta_de_2_menos_5(self):
+        resultado = self.calc.resta(2, 5)
+        self.assertEqual(-3, resultado)
 
 
-
-if __name__=="__main__":
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
